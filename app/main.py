f046d5ba01afe6db70e9ec1262a1da8c075c7788
Santtana <159819303+S4NTTANA@users.noreply.github.com>
2024-10-28 12:40:40 +0000

    Add usuario_service - Python

---
+++ app/main.py
@@ -0,0 +1,25 @@
+from services.usuario_service import UsuarioService
+from repositories.usuario_repositories import UsuarioRepository
+from config.database import Session
+
+def main():
+    session = Session()
+    repository = UsuarioRepository(session)
+    service = UsuarioService(repository)
+
+    # Solicitando dados para o usuário.
+    print("\nAdicionando usuário.")
+    nome = input("Digite seu nome: ")
+    email = input("Digite seu e-mail: ")
+    senha = input("Digite sua senha: ")
+
+    service.criar_usuario(nome=nome, email=email, senha=senha)
+
+    # Listar todos os usuário cadastrados.
+    print("\nListando usuarios cadastrados.")
+    lista_usuarios = service.listar_todos_usuarios()
+    for usuario in lista_usuarios:
+        print(f"Nome: {usuario.nome} - Email: {usuario.email} - Senha: {usuario.senha}")
+
+if __name__ == "__main__":
+    main()
---
+++ app/services/usuario_service.py
@@ -0,0 +1,26 @@
+from models.usuario_model import Usuario
+from repositories.usuario_repositories import UsuarioRepository
+
+class UsuarioService:
+    def __init__(self, repository: UsuarioRepository):
+        self.repository = repository
+
+    def criar_usuario(self, nome: str, email: str, senha: str):
+        try: 
+            usuario = Usuario(nome=nome, email=email, senha=senha)
+
+            novo_usuario = self.repository.pesquisar_usuario_por_email(usuario.email)
+            
+            if not novo_usuario:
+                print("Usuario já cadastrado!")
+                return
+
+            self.repository.salvar_usuario(usuario)
+            print("Usuário cadastrado com sucesso!")
+        except TypeError as erro:
+            print(f"Erro ao salvar o usuário: {erro}")
+        except Exception as erro:
+            print(f"Ocorreu um erro inesperado: {erro}")
+
+def listar_todos_usuarios(self):
+    return self.repository.listar_usuarios()
